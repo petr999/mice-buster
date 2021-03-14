e312a9f475ca284f47df02f08fee65f52b406953
Peter Vereshagin <peter@vereshagin.org>
2021-03-14 11:33:08 +0400

    feat(cat): mouse jump timeout

--- cat/src/controllers/cat.controller.ts
+++ cat/src/controllers/cat.controller.ts
@@ -1,16 +1,27 @@
 import {inject} from '@loopback/core';
+import {AnyObject} from '@loopback/repository';
 import {get, param} from '@loopback/rest';
 import {Mouse, MouseService} from '../services';
+
+/**
+ * Constants
+ */
+// Maximum number of seconds to delay the jump for lure
+const DELAY_SECONDS_MAX = 3
 
 export const MouseSchema = {
   type: 'object',
   required: ['id'],
   properties: {
-    id: {type: 'number'},
-    date: {type: 'string'},
-    delay: {type: 'number'},
+    queueId: {type: 'number'},
+    body: {type: 'object', partial: true, },
   },
 };
+
+export type MouseResponse = {
+  queueId: number;
+  body?: Mouse;
+}
 
 export class CatController {
   constructor(
@@ -27,9 +38,30 @@ export class CatController {
       },
     },
   })
-  async getMouseById(@param.path.number('mouseId') mouseId: number): Promise<Mouse> {
-    const response = await this.mouseService.lure({lureId: mouseId});
-    return response.body;
+  async getMouseById(@param.path.number('mouseId') mouseId: number): Promise<MouseResponse> {
+    const mouseResponsePromise = this.mouseService.lure({lureId: mouseId})
+    const timeoutPromise = this.getTimeoutPromise()
+    const mouseResponse = await Promise.race([mouseResponsePromise, timeoutPromise])
+    const body = mouseResponse.body
+    const queueId = await this.getQueueId()
+    const response: MouseResponse = {queueId}
+    if ('object' == typeof (body)) {response.body = body}
+    return response
+  }
+
+  // Mock one
+  async getQueueId() {
+    let rv = Math.random()
+    rv *= 1000000
+    rv = Math.floor(rv)
+
+    return rv
+  }
+
+  // Mock one
+  async getTimeoutPromise(): Promise<AnyObject> {
+    return new Promise(resolve => setTimeout(() => {resolve({});}, 1000 * DELAY_SECONDS_MAX))
+
   }
 
 }
